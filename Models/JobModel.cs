ca62b3ea74aff6c37920bc0d9c1f09d9ee021c44
Abimbola <craftwordltd@gmail.com>
2020-05-10 14:15:17 +0100

    create Job Model

---
+++ Models/JobModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecruitmentPortalApp.Models
+{
+    public class JobModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string Code { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Roles { get; set; }
+        public string Category { get; set; }
+        public DateTime ClosingDate { get; set; }
+
+
+    }
+}
