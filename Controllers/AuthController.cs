d0a467c5de0ab4b38d5c88b0fba9dd261a70ae29
Abimbola <craftwordltd@gmail.com>
2020-05-12 03:38:56 +0100

    create login Models

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using RecruitmentPortalApp.Models;
+
+namespace RecruitmentPortalApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<UserModel> _userManager;
+        private readonly SignInManager<UserModel> _signInManager;
+        private readonly IConfiguration _configuration;
+        public AuthController(UserManager<UserModel> userManager,
+            SignInManager<UserModel> signInManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _configuration = configuration;
+        }
+
+        [Route("register")]
+        [HttpPost]
+        public async Task<ActionResult> NewUser(LoginModel model)
+        {
+            var user = new UserModel
+            {
+                Email = model.Email,
+                UserName = model.Email
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                var idResult = await _userManager.AddToRoleAsync(user, "Customer");
+                return Ok(new { Username = user.UserName, userCreated = true });
+            }
+
+            return BadRequest(result.Errors);
+        }
+
+        [Route("login")]
+        [HttpPost]
+        public async Task<ActionResult> LoginUser(LoginModel model)
+        {
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var roles = await _userManager.GetRolesAsync(user);
+                List<Claim> claims = new List<Claim>() {
+                    new Claim(JwtRegisteredClaimNames.Sub, user.UserName)
+                    //new Claim(ClaimTypes.Name, user.Id.ToString()),
+                    
+                };
+
+                claims.AddRange(roles.Select(role => new Claim(ClaimsIdentity.DefaultRoleClaimType, role)));
+
+                var signinKey = new SymmetricSecurityKey(
+                  Encoding.UTF8.GetBytes(_configuration["Jwt:SigningKey"]));
+
+                int expiryInMinutes = Convert.ToInt32(_configuration["Jwt:ExpiryInMinutes"]);
+
+                var token = new JwtSecurityToken(
+                  issuer: _configuration["Jwt:Site"],
+                  audience: _configuration["Jwt:Site"],
+                  claims: claims,
+                  expires: DateTime.UtcNow.AddMinutes(expiryInMinutes),
+                  signingCredentials: new SigningCredentials(signinKey, SecurityAlgorithms.HmacSha256)
+                );
+
+                return Ok(
+                  new
+                  {
+                      token = new JwtSecurityTokenHandler().WriteToken(token),
+                      expiration = token.ValidTo,
+                      claim = token.Claims,
+                      userEmail = user.Email,
+                      userId = user.Id
+                  });
+            }
+            return Unauthorized();
+        }
+
+        //Logout out signed in user
+        [Route("logout")]
+        [HttpGet]
+        public async Task<ActionResult> Logout()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                await _signInManager.SignOutAsync();
+            }
+
+            return Ok();
+        }
+
+    }
+}
