248bc88e8db462e4dbf813fa44c0154a05674e72
Abimbola <craftwordltd@gmail.com>
2020-05-12 17:41:47 +0100

    add role controller

---
+++ Controllers/JobController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecruitmentPortalApp.Controllers
+{
+    public class JobController
+    {
+    }
+}
